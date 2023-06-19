18587c7851cf2651a2d356c135f94bfe397a68d8
Sergey Chv <serge.techno@gmail.com>
2023-06-19 23:19:17 +0300

    added typescript trainy - base part

--- ip/test.ts
+++ ip/test.ts
@@ -202,3 +202,92 @@ const nums12: number[][] = [[3,5,6], [3,5,8]];
 // destruct
 const [first] = report;
 console.log(first);
+
+
+// КОРТЕЖИ - TUPPLE - записываем набор данных в строго определенныом порядке и нужных типов
+
+const userData8 = {
+  isBirthdayData: true,
+  ageData: 40,
+  userName3: 'Serge',
+}
+
+const userDataTuple: [boolean, number, string] = [true, 40, 'Serge'];
+
+
+// UNION ОБЪЕДИНЕНИЕ ТИПОВ - из нескольких простых типов
+const message: string | number = 5; // или один тип или другой
+const messages: string[] | number[] = ['a', 'b'] // строго один тип в массиве 
+
+function printMsg2 (msg: string | number): void {
+  console.log(msg)
+}
+
+printMsg2(4);
+printMsg2('hello');
+
+// СУЖЕНИЕ ТИПОВ - NARROWING
+
+function printMsg3 (msg: string | number): void {
+
+  // делаем механизм
+  if (typeof msg === 'string') {
+    console.log(msg.toLowerCase());
+  } else {
+    console.log(msg.toExponential());
+  }
+  
+  // вне механизма
+  console.log(msg)
+}
+
+printMsg3(4);
+printMsg3('hello');
+
+
+// 2 option
+function printMsg4 (msg: string[] | number | boolean): void {
+
+  // делаем механизм
+  if (Array.isArray(msg)) {
+    msg.forEach(m => console.log(m))
+  } else if (typeof msg === 'number'){
+    console.log(msg.toFixed());
+  } else {
+    console.log(msg)
+  }
+  
+  // вне механизма
+  console.log(msg)
+}
+
+printMsg4(4);
+
+// next example
+const printReadings = (a: number | string, b: number | boolean) => {
+  if (a === b) {
+    console.log(a, b)
+  }
+}
+
+const printReadings2 = (a: number[] | string) => {
+    console.log(a.slice(0,3))
+}
+
+//
+const checkReadings = (readings: {system: number} | {user: number}): void {
+  if ('system' in readings) {
+    console.log(readings.system)
+  } else {
+    console.log(readings.user)
+  }
+}
+
+function logValue (x: string | Date) {
+  if(x instanceof Date) {
+    console.log(x.getDay())
+  } else {
+
+    console.log(x.includes('e'))
+  }
+}
