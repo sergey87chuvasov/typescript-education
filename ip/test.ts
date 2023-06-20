693773b785e6dbdcb21fe8747796eb7c5665208c
Sergey Chv <serge.techno@gmail.com>
2023-06-20 23:13:02 +0300

    added typescript trainy - base level

--- ip/test.ts
+++ ip/test.ts
@@ -147,7 +147,7 @@ const trimData = data5 ? data5.trim() : null;
 // und
 const test8: undefined = undefined;
 const test9: any = undefined;
-const test10: string = undefined;
+// const test10: string = undefined;
 
 let smth; // any
 
@@ -160,9 +160,9 @@ const data8 = {
 }
 console.log(data8[id]);
 
-const num1: bigint = 1n;
-const num2: bigint = 2n;
-console.log(num1 + num2);
+// const num1: bigint = 1n;
+// const num2: bigint = 2n;
+// console.log(num1 + num2);
 
 
 // ТИПИЗИЦИЯ ОБЪЕКТОВ И ДЕСТРУКТУРИЗАЦИЯ
@@ -275,7 +275,7 @@ const printReadings2 = (a: number[] | string) => {
 }
 
 //
-const checkReadings = (readings: {system: number} | {user: number}): void {
+const checkReadings = (readings: {system: number} | {user: number}): void => {
   if ('system' in readings) {
     console.log(readings.system)
   } else {
@@ -291,3 +291,173 @@ function logValue (x: string | Date) {
     console.log(x.includes('e'))
   }
 }
+
+// ПРИМИТИВНЫЕ ЛИТЕРАЛЬНЫЕ ТИПЫ
+let msg: 'Hello' = 'Hello';
+msg = 'Hello'; // ЭТО И ЕСТЬ литеральный тип - четкая переменная - это просто значение котрое мы с вами 
+// устанавливаем для чего либо и строго ему соответствуем
+
+//
+const port3000: number = 3000;
+const port3001: number = 3001;
+
+function startServer(protocol: 'http' | 'https', port: 3000 | 3001): 'Server started' {
+  if(port === port3000 || port === port3001) {
+    console.log(`Server started on ${protocol}://server:${port}`)
+  } else {
+    console.error('invalid port')
+  }
+  
+  return 'Server started'
+}
+startServer('https', 3001); // Server started on https:// server: 3001
+
+//
+
+type AnimationTimingFunc = 'ease' | 'ease-out' | 'ease-in';
+type AnimationID = string | number;
+
+function createAnimation(id: AnimationID , animationName: string, 
+  timingFunc: AnimationTimingFunc = 'ease',
+  duration: number, iterCount: 'infinite' | number
+  ): void {
+  // const elem = document.querySelector(`${id}`) as HTMLElement;
+
+  // if(elem) {
+    console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`)
+    // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+  // }
+  
+}
+
+createAnimation('id','fade','ease-in',6,'infinite'); // fade ease-in 6 infinite
+
+// ПСЕВДОНИМЫ ТИПОВ - TYPE ALIASES
+// type AnimationTimingFunc = 'ease' | 'ease-out' | 'ease-in';
+// timingFunc: 'ease' | 'ease-out' | 'ease-in' = 'ease' = AnimationTimingFunc
+
+
+// ОБЪЕКТНЫЕ ЛИТЕРАЛЫ И АННОТАЦИИ ФУНКЦИЙ
+
+const serverConfig2: {protocol: 'http' | 'https'; port: 3000 | 3001} = {
+  protocol: 'https',
+  port: 3001,
+}
+
+// нужно уметь различать аннотацию функции от ее объявления
+const startServer2: (protocol: 'http' | 'https', port: 3000 | 3001) => string 
+= (protocol: 'http' | 'https', port: 3000 | 3001): 'Server started' => {
+  
+    console.log(`Server started on ${protocol}://server:${port}`)
+  
+  return 'Server started'
+}
+startServer2(serverConfig2.protocol, serverConfig2.port); // Server started on https:// server: 3001
+
+//
+
+type AnimationTimingFunc2 = 'ease' | 'ease-out' | 'ease-in';
+type AnimationID2 = string | number;
+
+function createAnimation2(id: AnimationID2 , animationName: string, 
+  timingFunc: AnimationTimingFunc2 = 'ease',
+  duration: number, iterCount: 'infinite' | number
+  ): void {
+  // const elem = document.querySelector(`${id}`) as HTMLElement;
+
+  // if(elem) {
+    console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`)
+    // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+  // }
+  
+}
+
+createAnimation2('id','fade','ease-in',6,'infinite'); // fade ease-in 6 infinite
+
+// ТЕСТ
+
+// 1-  тип never можно получить если - функция никогда не заканчивается возвращаемым значением
+
+/* 2- 
+Какая ошибка допущена в этом коде?
+let salary;
+salary = 40000;
+ответ: salary имеет тип any даже после присвоения значения
+ */
+
+// 3 - Union type позволяет сказать коду, что сущность будет одним из педложенных типов
+// 4 - Кортеж (tuple) - это структура, которая необходима для записи набора данных в строго определенном порядке по типам
+// 5 - Механизм, когда мы определяем тип для того, чтобы выполнить с ним нужные операции называется - Сужение типов (Narrowing)
+// 6 - Какой оператор или метод невозможно использовать для сужения типов? - Object.is()
+// 7 - 
+/*
+Есть ли проблемы в этом коде?
+
+const departments: string[] = ["dev", "design", "marketing"];
+const department = departments[0];
+department.parseInt();
+ответ: ДА, В переменной department ледит строка а у строки нет метода parseint()
+*/
+
+// Какой из вариантов указания возвращаемого значения не доступен в функциях?
+// function test(): []number {}
+
+// Как называется данный прием? type AnimationTimingFunc = "ease" | "ease-out" | "ease-in"; - Псевдоним типа (Type Aliase)
+// 10 - Литерал это - конкретное значение любого типа'
+
+/*
+Вопрос 11:
+Какой тип будет в указанной строке? (7я строка)
+
+function printMsg(msg: string[] | number | boolean): void {
+    if (Array.isArray(msg)) {
+        msg.forEach((m) => console.log(m));
+    } else if (typeof msg === "number") {
+        console.log(msg.toFixed());
+    } else {
+        console.log(msg); // Вот здесь! boolean
+    }
+}
+*/
+
+/*
+Вопрос 12:
+Какие типы будут у переменных a и b последовательно в указанной строке?
+
+const printReadings = (a: number | string, b: number | boolean) => {
+    if (a === b) {
+        console.log(a, b); // Вот здесь! [number, number]
+    }
+};
+*/
+
+/*
+Вопрос 13:
+Какой тип будет у переменной res в данном коде?
+
+const userDataTuple: [...boolean[], number, string] = [true, false, 40, "John"];
+const res = userDataTuple.map(d => d.toString());
+
+Ответ: - Да, метод toString() существует на всех этих типах и превращает каждый в строку. 
+А значит, мы получим новый массив, состоящий полностью из строк.
+*/
+
+/*
+Вопрос 14:
+Какая проблема в этом коде?
+
+const printReadings = (a: number[] | string) => {
+    console.log(a.slice(0, 3));
+};
+
+оТВЕТ: Метод slice существует на этих типах проблем не будет
+*/
+
+/*
+Вопрос 15:
+Какой тип у переменной port3002 ?
+
+const port3000: number = 3000;
+const port3001 = 3001;
+let port3002 = 3002; - NUMBER
+*/
