4e5ec35572fe310d3a5577a48f8aae0487de305e
Sergey87 <serge.techno@gmail.com>
2023-06-28 00:51:30 +0300

    added typescript training

--- mn/lections/src/3-advanced.ts
+++ mn/lections/src/3-advanced.ts
@@ -76,3 +76,93 @@ type Union7 = { a: string; b: string; c: number } & {
 // 1. when we want expand interface in the fiture
 // 2. class with implements
 // 3. prefix I
+
+// СУЖЕНИЕ ТИПОВ - nARROWING
+
+function example1(x?: number | string) {
+  if (typeof x === 'string') {
+    x.toLowerCase();
+  } else if (typeof x === 'number') {
+    x.toFixed();
+  } else if (x === undefined) {
+    console.log('no value');
+  } else {
+    x;
+  }
+}
+
+function example2(strs: string | string[] | null) {
+  // if(typeof strs === 'object'){}
+  // if (Array.isArray(strs)) {}
+  if (strs && typeof strs === 'object') {
+    strs.concat([]);
+  } else if (typeof strs === 'string') {
+    strs.toLocaleLowerCase();
+  }
+}
+
+function example3(x: number[] | Date) {
+  if (x instanceof Date) {
+    x.getDate();
+  } else {
+    x.concat([]);
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function move(animal: Fish | Bird) {
+  if ('swim' in animal) {
+    return animal.swim();
+  }
+
+  return animal.fly();
+}
+
+// TYPE GUARDS
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function move2(animal: Fish | Bird) {
+  if (isFish(animal)) {
+    return animal.swim();
+  }
+
+  return animal.fly();
+}
+
+// ASSERTS
+type User3 = {
+  name: string;
+  displayName: string | null;
+};
+
+function assertDisplayName(
+  user: User3
+): asserts user is User3 & { displayName: string } {
+  if (!user.displayName) throw new Error('error - user has no display name');
+}
+
+function logUserByDisplayName(user: User3) {
+  assertDisplayName(user);
+
+  console.log(user.displayName.toUpperCase());
+}
+
+// ТИПИЗАЦИЯ THIS
+
+const user4 = {
+  id: 1243,
+  admin: false,
+  becomeAdmin: function () {
+    this.admin = true;
+  },
+};
+
+// конкретизируем что тае тут this
+function myClickHandler(this: HTMLButtonElement, event: Event) {
+  this.disabled = true;
+}
--- mn/projects/src/3-advance.ts
+++ mn/projects/src/3-advance.ts
@@ -54,3 +54,40 @@ type Car = Product &
   };
 
 const car1: Car = {};
+
+// Напишите две функции типа type guards.
+// Опишите функции type guards и выведите их типы.
+
+/*
+Для чего служит оператор as?
+Оператор as позволяет сделать утверждение, что та или иная переменная является конкретным типом.
+Он позволяет делать сужение типа для any и unknown, а также для union типов.
+*/
+
+interface Order {
+  address: string;
+}
+interface TelephoneOrder extends Order {
+  callerNumber: string;
+}
+interface InternetOrder extends Order {
+  email: string;
+}
+
+type PossibleOrders = TelephoneOrder | InternetOrder | undefined;
+
+function isAnInternetOrder(order: PossibleOrders): order is InternetOrder {
+  return !!order && 'email' in order;
+}
+
+function isATelephoneOrder(order: PossibleOrders): order is TelephoneOrder {
+  return !!order && 'callerNumber' in order;
+}
+
+function makeOrder(order: PossibleOrders) {
+  if (isAnInternetOrder(order)) {
+    console.log(order.email);
+  } else if (isATelephoneOrder(order)) {
+    console.log(order.callerNumber);
+  }
+}
